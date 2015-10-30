5690e5dce4cf402dc088b9f63e4c14275c2a7d49
dan <dcwolf@gmail.com>
2015-10-30 17:15:49 -0400

    second commit of initial Kivy interface

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 #!/usr/bin/kivy
 from kivy.app import App
 from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.factory import Factory
+from os.path import join
 
 
-class ItemEditorWidget(TabbedPanel):
-    pass
+class Root(TabbedPanel):
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_load(self):
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(join(path, filename[0])) as stream:
+            pass  # TODO: This will be where I process the file
+
+        self.dismiss_popup()
+
+
+class LoadDialog(FloatLayout):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class ItemEditorApp(App):
-    def build(self):
-        return ItemEditorWidget()
+    pass
+
+
+Factory.register('Root', cls=Root)
+Factory.register('LoadDialog', cls=LoadDialog)
 
 if __name__ == '__main__':
     ItemEditorApp().run()
